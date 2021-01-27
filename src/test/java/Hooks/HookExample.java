5a9d2e592739b80386e28a4099621797cb6d6796
DurgaTV <DurgaTV>
2021-01-27 22:52:06 +0100

    changes in hookfile

--- src/test/java/Hooks/HookExample.java
+++ src/test/java/Hooks/HookExample.java
@@ -1,23 +1,23 @@
-/*
- * package Hooks;
- * 
- * import io.cucumber.java.After; import io.cucumber.java.Before; import
- * io.cucumber.java.Scenario;
- * 
- * public class HookExample {
- * 
- * @Before("@smoke") public void beforeHookExecution(Scenario sc) {
- * System.out.println("Before hook statement1");
- * System.out.println(sc.getUri()); System.out.println(sc.getLine());
- * System.out.println(sc.getSourceTagNames());
- * System.out.println(sc.getStatus()); }
- * 
- * @After(order=2) public void afterHookExecution() {
- * System.out.println("After hook statement"); }
- * 
- * @Before(order=5) public void befHookExecution() {
- * System.out.println("before each hook statement2"); }
- * 
- * @After(order=1) public void aftHookExecution() {
- * System.out.println("After each hook statement2"); } }
- */
+
+  package Hooks;
+  
+  import io.cucumber.java.After; import io.cucumber.java.Before; import
+  io.cucumber.java.Scenario;
+  
+  public class HookExample {
+  
+  @Before("@smoke") public void beforeHookExecution(Scenario sc) {
+  System.out.println("Before hook statement1");
+  System.out.println(sc.getUri()); System.out.println(sc.getLine());
+  System.out.println(sc.getSourceTagNames());
+  System.out.println(sc.getStatus()); }
+  
+  @After(order=2) public void afterHookExecution() {
+  System.out.println("After hook statement"); }
+  
+  @Before(order=5) public void befHookExecution() {
+  System.out.println("before each hook statement2"); }
+  
+  @After(order=1) public void aftHookExecution() {
+  System.out.println("After each hook statement2"); } }
+ 
